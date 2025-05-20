b45e5c4a1321d7d05cadf3336ab36850811119a4
Timothée <timotheeithier@gmail.com>
2025-05-20 16:07:53 +0200

    le test est bon

--- test.c
+++ test.c
@@ -6,19 +6,23 @@ int main() {
     printf("Quelle est la hauteur du sapin ? 🎄\n");
     scanf("%d", &height);
 
-    for (int i = 1; i <= height; i++) {
-        // Imprimer les espaces
-        for (int j = 1; j <= height - i; j++) {
+    for (int LineNum = 1; LineNum <= height; LineNum++) {
+        // Affichage du feuillage
+        for (int j = 0; j < height - LineNum; j++) {
             printf(" ");
         }
-
-        // Imprimer les étoiles (2*i - 1 étoiles par ligne)
-        for (int k = 1; k <= 2 * i - 1; k++) {
+        for (int k = 0; k < 2 * LineNum - 1; k++) {
             printf("*");
         }
-
-        // Retour à la ligne
         printf("\n");
+    }
+
+    // Affichage du tronc avec IF
+    if (height > 0) {
+        for (int j = 0; j < height - 1; j++) {
+            printf(" ");
+        }
+        printf("||\n");
     }
 
     return 0;
