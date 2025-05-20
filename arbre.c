fa8e3359a965632d0ef1872127eb935711cfd809
Timothée <timotheeithier@gmail.com>
2025-05-20 15:43:53 +0200

    on a un arbre, il faut faire le tronc

--- arbre.c
+++ arbre.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 
-
-
-
 int main()
 {
     int LineNum;
+
     printf("how many line: \n");
     scanf("%d", &LineNum);
-    for (int i = 0 ; i <= LineNum; i++) {
-        for (int j = 1; j <= i; j++){
+
+    for (int i = 1 ; i <= LineNum; i++) {
+
+        for (int k = 1; k <= LineNum - i; k++){
+            printf(" ");
+            }
+
+
+
+        for (int j = 2; j <= 2 * i; j++){
             printf("*");
-        }
+
+            }
+        
         printf("\n");
+
     }
-    printf("||");
+    printf("||")
+    ;
     return 0;
 
     }
