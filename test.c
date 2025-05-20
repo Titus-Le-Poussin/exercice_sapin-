2fe2097a0cced58fbcb75f83d90f34dd41bceeee
Timothée <timotheeithier@gmail.com>
2025-05-20 15:50:50 +0200

    le test fonctionne

--- test.c
+++ test.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 0; i < 5; i++) {
-        printf("Tour numéro %d\n", i);
+    int height;
+
+    printf("Quelle est la hauteur du sapin ? 🎄\n");
+    scanf("%d", &height);
+
+    for (int i = 1; i <= height; i++) {
+        // Imprimer les espaces
+        for (int j = 1; j <= height - i; j++) {
+            printf(" ");
+        }
+
+        // Imprimer les étoiles (2*i - 1 étoiles par ligne)
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            printf("*");
+        }
+
+        // Retour à la ligne
+        printf("\n");
     }
+
     return 0;
 }
